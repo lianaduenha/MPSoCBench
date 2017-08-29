3421e779d63f78ce6e4676fd6225ccef090c4e3c
Thiago <noscombr@hotmail.com>
2017-08-29 11:55:19 -0400

    Router.het.lt agora armazena os processadores em vector. Modificações para generalizar as conexoes e binds.

--- platforms/platform.router.het.lt/main.cpp
+++ platforms/platform.router.het.lt/main.cpp
@@ -64,8 +64,6 @@ using user::tlm_dfs;
 #endif
 
 
-
-
 // Global variables
 int N_WORKERS;
 struct timeval startTime;
@@ -78,12 +76,6 @@ bool first_load;
 // Functions
 void report_start(char *, char *, char *);
 void report_end();
-//void load_elf(mips300 &, tlm_memory &, char *, unsigned int, unsigned int);
-//void load_elf(mips800 &, tlm_memory &, char *, unsigned int, unsigned int);
-//void load_elf300(PROCESSOR_NAME300 &, tlm_memory &, char *, unsigned int, unsigned int);
-//void load_elf800(PROCESSOR_NAME800 &, tlm_memory &, char *, unsigned int, unsigned int);
-
-//template<typename proc> void load_elf(proc &, tlm_memory &, char *, unsigned int, unsigned int);
 
 template<class type_core> void load_elf(type_core &, tlm_memory &, char *, unsigned int, unsigned int);
 
@@ -107,12 +99,35 @@ int sc_main (int ac, char *av[])
     exit(1);
   }
 
+  int id = 0;
 
-  mips300 *proc1 = new mips300("proc1", 0);
-  mips300 *proc2 = new mips300("proc2", 1);
+  int N_CORES_300 = 2;
+  int N_CORES_800 = 2;
 
-  mips800 *proc3 = new mips800("proc3", 2);
-  mips800 *proc4 = new mips800("proc4", 3);
+
+  vector<mips300 *> procs_300;
+
+  
+  for (int i = 0; i < N_CORES_300; i++) {
+    char name[10] = "proc";
+    char number_str[3];
+    sprintf(number_str, "%d", id);
+    strcat(name, number_str);
+    procs_300.push_back(new mips300(name, id));
+    id++;
+  }
+
+  vector<mips800 *> procs_800;
+
+  for (int i = 0; i < N_CORES_800; i++) {
+    char name[10] = "proc";
+    char number_str[3];
+    sprintf(number_str, "%d", id);
+    strcat(name, number_str);
+    procs_800.push_back(new mips800(name, id));
+    id++;
+  }
+    
 
   // Platform components
   tlm_memory mem("mem", 0, MEM_SIZE - 1); // memory
@@ -121,181 +136,172 @@ int sc_main (int ac, char *av[])
   tlm_intr_ctrl intr_ctrl("intr_ctrl", N_WORKERS);
   //tlm_diretorio dir("dir");
   #ifdef POWER_SIM
-  //tlm_dfs dfs("dfs", N_WORKERS, processors); // dfs
+    //tlm_dfs dfs("dfs", N_WORKERS, processors); // dfs
   #endif
 
 
- // Binding ports
+  // Binding ports
   router.MEM_port(mem.target_export);
   router.LOCK_port(locker.target_export);
   router.INTR_CTRL_port(intr_ctrl.target_export);
   //router.DIR_port(dir.target_export);
 
   #ifdef POWER_SIM
-  //router.DFS_port(dfs.target_export);
+    //router.DFS_port(dfs.target_export);
   #endif
 
+    
+
   // Initializing Memports
+  for (int i = 0; i < N_CORES_300; i++) {
+    procs_300[i]->MEM(router.target_export);
+    (procs_300[i]->MEM_mport).setProcId(procs_300[i]->getId());
+  }
+
+  for (int i = 0; i < N_CORES_800; i++) {
+    procs_800[i]->MEM(router.target_export);
+    (procs_800[i]->MEM_mport).setProcId(procs_800[i]->getId());
+  }
   
-  proc1->MEM(router.target_export);
-  (proc1->MEM_mport).setProcId(proc1->getId());  
-
-  proc2->MEM(router.target_export);
-  (proc2->MEM_mport).setProcId(proc2->getId());
-
-  proc3->MEM(router.target_export);
-  (proc3->MEM_mport).setProcId(proc3->getId());  
-
-  proc4->MEM(router.target_export);
-  (proc4->MEM_mport).setProcId(proc4->getId());
-  
+    
+    
 
     // Binding processors and interruption controller
-  
-  intr_ctrl.CPU_port[0](proc1->intr_port);
-  intr_ctrl.CPU_port[1](proc2->intr_port);
+    int aux = 0;
+    for (int i = 0; i < N_CORES_300; i++) {
+      intr_ctrl.CPU_port[aux](procs_300[i]->intr_port);
+      aux++;
+    }
 
-  intr_ctrl.CPU_port[2](proc3->intr_port);
-  intr_ctrl.CPU_port[3](proc4->intr_port);
+    for (int i = 0; i < N_CORES_800; i++) {
+      intr_ctrl.CPU_port[aux](procs_800[i]->intr_port);
+      aux++;
+    }
+    
 
 
     // Processor 0 starts simulatino in ON-mode while the other processors are in
     // OFF-mode
-  for (int i = 1; i < N_WORKERS; i++) {
-    intr_ctrl.send(i, OFF);
-  }
-  intr_ctrl.send(0, ON); // turn on processor 0 (master)
-
-  // Preparing the arguments
-  char **arguments[N_WORKERS];
-  for (int i = 0; i < N_WORKERS; i++) {
-    arguments[i] = (char **)new char *[ac];
-  }
-
-  for (int i = 0; i < N_WORKERS; i++) {
-    for (int j = 0; j < ac; j++) {
-      arguments[i][j] = new char[strlen(av[j]) + 1];
-      arguments[i][j] = av[j];
+    for (int i = 1; i < N_WORKERS; i++) {
+      intr_ctrl.send(i, OFF);
     }
-  }
+    intr_ctrl.send(0, ON); // turn on processor 0 (master)
 
+    // Preparing the arguments
+    char **arguments[N_WORKERS];
+    for (int i = 0; i < N_WORKERS; i++) {
+      arguments[i] = (char **)new char *[ac];
+    }
 
-  // Load elf before start
-  first_load = true;
-  load_elf<mips300>(*proc1, mem, arguments[0][1], 0x000000, MEM_SIZE);
+    for (int i = 0; i < N_WORKERS; i++) {
+      for (int j = 0; j < ac; j++) {
+        arguments[i][j] = new char[strlen(av[j]) + 1];
+        arguments[i][j] = av[j];
+      }
+    }
 
-  first_load = false;
-  load_elf<mips300>(*proc2, mem, arguments[1][1], 0x000000, MEM_SIZE);
+    // Load elf before start
+    first_load = true; aux = 0;
+    for (int i = 0; i < N_CORES_300; i++) {
+      load_elf<mips300>(*procs_300[i], mem, arguments[aux][1], 0x000000, MEM_SIZE);
+      first_load = false; aux++;
+    }
 
-  first_load = false;
-  load_elf<mips800>(*proc3, mem, arguments[2][1], 0x000000, MEM_SIZE);
-
-  first_load = false;
-  load_elf<mips800>(*proc4, mem, arguments[3][1], 0x000000, MEM_SIZE);
-
-  /*irst_load = true;
-  load_elf300(*proc1, mem, arguments[0][1], 0x000000, MEM_SIZE);
-
-  first_load = false;
-  load_elf800(*proc2, mem, arguments[1][1], 0x000000, MEM_SIZE);*/
-
-  proc1->init();
-  proc2->init();
-
-  proc3->init();
-  proc4->init();
-
-
-  report_start(av[0], av[1], av[2]);
-
-  // Beggining of simulation
-
-  
-  sc_start();
-
-  proc1->PrintStat();
-  proc1->FilePrintStat(global_time_measures);
-  proc1->FilePrintStat(local_time_measures);
-
-  proc2->PrintStat();
-  proc2->FilePrintStat(global_time_measures);
-  proc2->FilePrintStat(local_time_measures);
-
-  proc3->PrintStat();
-  proc3->FilePrintStat(global_time_measures);
-  proc3->FilePrintStat(local_time_measures);
-
-  proc4->PrintStat();
-  proc4->FilePrintStat(global_time_measures);
-  proc4->FilePrintStat(local_time_measures);
+    for (int i = 0; i < N_CORES_800; i++) {
+      load_elf<mips800>(*procs_800[i], mem, arguments[aux][1], 0x000000, MEM_SIZE);
+      first_load = false; aux++;
+    }
 
 
 
-  // Printing statistics
-#ifdef AC_STATS
-  
-  proc1->ac_sim_stats.time = sc_simulation_time();
-  proc1->ac_sim_stats.print();
+    for (int i = 0; i < N_CORES_300; i++) {
+      procs_300[i]->init(); // It passes the arguments to processors
+    }
 
-  proc2->ac_sim_stats.time = sc_simulation_time();
-  proc2->ac_sim_stats.print();
+    for (int i = 0; i < N_CORES_800; i++) {
+      procs_800[i]->init(); // It passes the arguments to processors
+    }
 
-  proc3->ac_sim_stats.time = sc_simulation_time();
-  proc3->ac_sim_stats.print();
 
-  proc4->ac_sim_stats.time = sc_simulation_time();
-  proc4->ac_sim_stats.print();
- 
-#endif
+    report_start(av[0], av[1], av[2]);
 
-#ifdef POWER_SIM
-  
-    // Connect Power Information from ArchC with PowerSC
-    /*proc1->ps.powersc_connect();
-    proc1->IC.powersc_connect();
-    proc1->DC.powersc_connect();
-
-    proc2->ps.powersc_connect();
-    proc2->IC.powersc_connect();
-    proc2->DC.powersc_connect();
-  
-  proc1->ps.report();*/
-#endif
-
-#ifdef POWER_SIM
-  /*double d = 0;
-
-  // Connect Power Information from ArchC with PowerSC
-  d += proc1->ps.getEnergyPerCore();
-  d += proc2->ps.getEnergyPerCore();
-  
-  printf("\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ", d * 0.000000001);
-  fprintf(local_time_measures, "\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ",
-          d * 0.000000001);
-  fprintf(global_time_measures, "\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ",
-          d * 0.000000001);*/
-#endif
-  
-// Checking the status
-  bool status;
-  
-  status = status + proc1->ac_exit_status;
-  status = status + proc2->ac_exit_status;
-
-  status = status + proc3->ac_exit_status;
-  status = status + proc4->ac_exit_status;
-
-  delete proc1;
-  delete proc2;
-  delete proc3;
-  delete proc4;
-
-  fclose(local_time_measures);
-  fclose(global_time_measures);
-
-  return status;
+    // Beggining of simulation
 
     
+    sc_start();
+
+    for (int i = 0; i < N_CORES_300; i++) {
+      procs_300[i]->PrintStat();
+      procs_300[i]->FilePrintStat(global_time_measures);
+      procs_300[i]->FilePrintStat(local_time_measures);
+    }
+
+    for (int i = 0; i < N_CORES_800; i++) {
+      procs_800[i]->PrintStat();
+      procs_800[i]->FilePrintStat(global_time_measures);
+      procs_800[i]->FilePrintStat(local_time_measures);
+    }
+
+
+    // Printing statistics
+    #ifdef AC_STATS
+
+      for (i = 0; i < N_CORES_300; i++) {
+        procs_300[i]->ac_sim_stats.time = sc_simulation_time();
+        procs_300[i]->ac_sim_stats.print();
+      }
+
+      for (i = 0; i < N_CORES_800; i++) {
+        procs_800[i]->ac_sim_stats.time = sc_simulation_time();
+        procs_800[i]->ac_sim_stats.print();
+      }
+
+     
+    #endif
+
+    #ifdef POWER_SIM
+      
+        // Connect Power Information from ArchC with PowerSC
+        /*proc1->ps.powersc_connect();
+        proc1->IC.powersc_connect();
+        proc1->DC.powersc_connect();
+
+        proc2->ps.powersc_connect();
+        proc2->IC.powersc_connect();
+        proc2->DC.powersc_connect();
+      
+      proc1->ps.report();*/
+    #endif
+
+    #ifdef POWER_SIM
+      /*double d = 0;
+
+      // Connect Power Information from ArchC with PowerSC
+      d += proc1->ps.getEnergyPerCore();
+      d += proc2->ps.getEnergyPerCore();
+      
+      printf("\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ", d * 0.000000001);
+      fprintf(local_time_measures, "\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ",
+              d * 0.000000001);
+      fprintf(global_time_measures, "\n\nTOTAL ENERGY (ALL CORES): %.10f J\n\n ",
+              d * 0.000000001);*/
+    #endif
+    
+    // Checking the status
+    bool status;
+
+    for (int i = 0; i < N_CORES_300; i++)
+      status = status + procs_300[i]->ac_exit_status;
+
+    for (int i = 0; i < N_CORES_800; i++)
+      status = status + procs_800[i]->ac_exit_status;
+    
+
+    fclose(local_time_measures);
+    fclose(global_time_measures);
+
+    return status;
+
 }
 
 
@@ -462,221 +468,3 @@ template<class type_core> void load_elf(type_core &proc, tlm_memory &mem, char *
 
   close(fd);
 }
-
-
-/*void load_elf800(PROCESSOR_NAME800 &proc, tlm_memory &mem, char *filename,
-              unsigned int offset, unsigned int memsize) {
-
-  Elf32_Ehdr ehdr;
-  Elf32_Shdr shdr;
-  Elf32_Phdr phdr;
-  int fd;
-  unsigned int i;
-  // unsigned int data_mem_size=(0x4FFFFF);
-  unsigned int data_mem_size = memsize;
-
-  if (!filename || ((fd = open(filename, 0)) == -1)) {
-    AC_ERROR("Openning application file '" << filename
-                                           << "': " << strerror(errno) << endl);
-    exit(EXIT_FAILURE);
-  }
-
-  // Test if it's an ELF file
-  if ((read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) || // read header
-      (strncmp((char *)ehdr.e_ident, ELFMAG, 4) != 0) || // test magic number
-      0) {
-    close(fd);
-    AC_ERROR("File '" << filename << "' is not an elf. : " << strerror(errno)
-                      << endl);
-    exit(EXIT_FAILURE);
-  }
-
-  // Set start address
-
-  proc.ac_start_addr = convert_endian(4, ehdr.e_entry, proc.ac_mt_endian);
-
-  if (proc.ac_start_addr > data_mem_size) {
-    printf("ac_start_addr: %d   data_mem_size: %d", proc.ac_start_addr,
-           data_mem_size);
-    AC_ERROR("the start address of the application is beyond model memory\n");
-    close(fd);
-    exit(EXIT_FAILURE);
-  }
-
-  if (convert_endian(2, ehdr.e_type, proc.ac_mt_endian) == ET_EXEC) {
-    // It is an ELF file
-    if (first_load)
-      AC_SAY("Reading ELF application file: " << filename << endl);
-
-    for (i = 0; i < convert_endian(2, ehdr.e_phnum, proc.ac_mt_endian); i++) {
-      // Get program headers and load segments
-      lseek(fd, convert_endian(4, ehdr.e_phoff, proc.ac_mt_endian) +
-                    convert_endian(2, ehdr.e_phentsize, proc.ac_mt_endian) * i,
-            SEEK_SET);
-      if (read(fd, &phdr, sizeof(phdr)) != sizeof(phdr)) {
-        AC_ERROR("reading ELF program header\n");
-        close(fd);
-        exit(EXIT_FAILURE);
-      }
-
-      if (convert_endian(4, phdr.p_type, proc.ac_mt_endian) == PT_LOAD) {
-        Elf32_Word j;
-        Elf32_Addr p_vaddr = convert_endian(4, phdr.p_vaddr, proc.ac_mt_endian);
-        Elf32_Word p_memsz = convert_endian(4, phdr.p_memsz, proc.ac_mt_endian);
-        Elf32_Word p_filesz =
-            convert_endian(4, phdr.p_filesz, proc.ac_mt_endian);
-        Elf32_Off p_offset =
-            convert_endian(4, phdr.p_offset, proc.ac_mt_endian);
-
-        // printf("data_mem_size: %d  application size: %d", data_mem_size,
-        // p_vaddr+p_memsz);
-        // Error if segment greater then memory
-        if (data_mem_size < p_vaddr + p_memsz) {
-          printf("data_mem_size: %d  application size: %d", data_mem_size,
-                 p_vaddr + p_memsz);
-          AC_ERROR("not enough memory in ArchC model to load application.\n");
-          close(fd);
-          exit(EXIT_FAILURE);
-        }
-
-        printf("MIPS800: ac_heap_ptr: %d\n", proc.ac_heap_ptr);
-        // Set heap to the end of the segment
-        if (proc.ac_heap_ptr < p_vaddr + p_memsz)
-          proc.ac_heap_ptr = p_vaddr + p_memsz;
-        if (!proc.dec_cache_size)
-          proc.dec_cache_size = proc.ac_heap_ptr;
-
-        // Load and correct endian
-        if (first_load) {
-          lseek(fd, p_offset, SEEK_SET);
-          for (j = 0; j < p_filesz;
-               j += sizeof(PROCESSOR_NAME800_parms::ac_word)) {
-            int tmp;
-            ssize_t ret_value =
-                read(fd, &tmp, sizeof(PROCESSOR_NAME800_parms::ac_word));
-            int d = convert_endian(sizeof(PROCESSOR_NAME800_parms::ac_word), tmp,
-                                   proc.ac_mt_endian);
-            mem.direct_write(&tmp, p_vaddr + j + offset);
-          }
-
-          int d = 0;
-          for (j = p_vaddr + p_filesz; j <= p_memsz - p_filesz; j++)
-            mem.direct_write(&d, p_vaddr + j);
-        } // if
-
-      } // if
-
-    } // for
-  } // if
-
-  close(fd);
-}
-
-void load_elf300(PROCESSOR_NAME300 &proc, tlm_memory &mem, char *filename,
-              unsigned int offset, unsigned int memsize) {
-
-  Elf32_Ehdr ehdr;
-  Elf32_Shdr shdr;
-  Elf32_Phdr phdr;
-  int fd;
-  unsigned int i;
-  // unsigned int data_mem_size=(0x4FFFFF);
-  unsigned int data_mem_size = memsize;
-
-  if (!filename || ((fd = open(filename, 0)) == -1)) {
-    AC_ERROR("Openning application file '" << filename
-                                           << "': " << strerror(errno) << endl);
-    exit(EXIT_FAILURE);
-  }
-
-  // Test if it's an ELF file
-  if ((read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) || // read header
-      (strncmp((char *)ehdr.e_ident, ELFMAG, 4) != 0) || // test magic number
-      0) {
-    close(fd);
-    AC_ERROR("File '" << filename << "' is not an elf. : " << strerror(errno)
-                      << endl);
-    exit(EXIT_FAILURE);
-  }
-
-  // Set start address
-
-  proc.ac_start_addr = convert_endian(4, ehdr.e_entry, proc.ac_mt_endian);
-
-  if (proc.ac_start_addr > data_mem_size) {
-    printf("ac_start_addr: %d   data_mem_size: %d", proc.ac_start_addr,
-           data_mem_size);
-    AC_ERROR("the start address of the application is beyond model memory\n");
-    close(fd);
-    exit(EXIT_FAILURE);
-  }
-
-  if (convert_endian(2, ehdr.e_type, proc.ac_mt_endian) == ET_EXEC) {
-    // It is an ELF file
-    if (first_load)
-      AC_SAY("Reading ELF application file: " << filename << endl);
-
-    for (i = 0; i < convert_endian(2, ehdr.e_phnum, proc.ac_mt_endian); i++) {
-      // Get program headers and load segments
-      lseek(fd, convert_endian(4, ehdr.e_phoff, proc.ac_mt_endian) +
-                    convert_endian(2, ehdr.e_phentsize, proc.ac_mt_endian) * i,
-            SEEK_SET);
-      if (read(fd, &phdr, sizeof(phdr)) != sizeof(phdr)) {
-        AC_ERROR("reading ELF program header\n");
-        close(fd);
-        exit(EXIT_FAILURE);
-      }
-
-      if (convert_endian(4, phdr.p_type, proc.ac_mt_endian) == PT_LOAD) {
-        Elf32_Word j;
-        Elf32_Addr p_vaddr = convert_endian(4, phdr.p_vaddr, proc.ac_mt_endian);
-        Elf32_Word p_memsz = convert_endian(4, phdr.p_memsz, proc.ac_mt_endian);
-        Elf32_Word p_filesz =
-            convert_endian(4, phdr.p_filesz, proc.ac_mt_endian);
-        Elf32_Off p_offset =
-            convert_endian(4, phdr.p_offset, proc.ac_mt_endian);
-
-        // printf("data_mem_size: %d  application size: %d", data_mem_size,
-        // p_vaddr+p_memsz);
-        // Error if segment greater then memory
-        if (data_mem_size < p_vaddr + p_memsz) {
-          printf("data_mem_size: %d  application size: %d", data_mem_size,
-                 p_vaddr + p_memsz);
-          AC_ERROR("not enough memory in ArchC model to load application.\n");
-          close(fd);
-          exit(EXIT_FAILURE);
-        }
-
-         printf("MIPS300: ac_heap_ptr: %d\n", proc.ac_heap_ptr);
-        // Set heap to the end of the segment
-        if (proc.ac_heap_ptr < p_vaddr + p_memsz)
-          proc.ac_heap_ptr = p_vaddr + p_memsz;
-        if (!proc.dec_cache_size)
-          proc.dec_cache_size = proc.ac_heap_ptr;
-
-        // Load and correct endian
-        if (first_load) {
-          lseek(fd, p_offset, SEEK_SET);
-          for (j = 0; j < p_filesz;
-               j += sizeof(PROCESSOR_NAME300_parms::ac_word)) {
-            int tmp;
-            ssize_t ret_value =
-                read(fd, &tmp, sizeof(PROCESSOR_NAME300_parms::ac_word));
-            int d = convert_endian(sizeof(PROCESSOR_NAME300_parms::ac_word), tmp,
-                                   proc.ac_mt_endian);
-            mem.direct_write(&tmp, p_vaddr + j + offset);
-          }
-
-          int d = 0;
-          for (j = p_vaddr + p_filesz; j <= p_memsz - p_filesz; j++)
-            mem.direct_write(&d, p_vaddr + j);
-        } // if
-
-      } // if
-
-    } // for
-  } // if
-
-  close(fd);
-}
-*/
